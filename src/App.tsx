ce2349c26d2caba70ef1f47c30ddba9782d8c225
munezaclovis <51137458+munezaclovis@users.noreply.github.com>
2022-10-11 01:07:18 +0000

    Build: (f0c735b) Update Node.js.yml

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-import { useRoutes } from "react-router-dom";
-import Routes from "./routes";
-
-function App() {
-    return useRoutes(Routes);
-}
-
-export default App;
