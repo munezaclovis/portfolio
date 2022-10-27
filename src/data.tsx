691c6c94bb756fcdd79889e6b8f770316f71b0ad
Clovis <51137458+munezaclovis@users.noreply.github.com>
2022-10-27 17:01:18 -0400

    added links and descriptions to projects

--- src/data.tsx
+++ src/data.tsx
@@ -25,13 +25,77 @@ export const ProjectsData: Array<DataSkeleton> = [
             "ECommerce site with user authentication and different access level authorization through roles and claims/permissions. Data management by admins and some ECommerce functionalities.",
         technologies: ["C#", ".NET", "Javascript", "MsSql", "Bootstrap", "Visual studio", "Nuget pachage manager"],
 
+        codeLink: "https://github.com/munezaclovis/ElectroStore",
         overview: (
             <>
-                <p className="project-details__desc-para">This project was</p>
-                <h3 className="project-details__content-title">Team Collaboration and division of work</h3>
+                <p className="project-details__desc-para">
+                    This idea was for a school project where each student had to choose a topic to make a project about.
+                    I choose to make an ECommerce similar website because I wanted to challenge myself and get a glimpse
+                    of what modern ecommerce websites work
+                </p>
+                <p className="project-details__desc-para">
+                    This project is written in .NET CORE 3, HTML, and Bootstrap for styling. The design of the website
+                    is from 3 different templates that I combined into 1 design for this project. I used Microsoft SQL
+                    as the database, and Entity Framework as ORM.
+                </p>
+                <h3 className="project-details__content-title">Discovery Phase</h3>
+                <p className="project-details__desc-para">
+                    During the discovery pahse, I researched and thought about technologies I'd be using, and how I was
+                    going to approach the project development. During our semester, we used .NET Framework. It wasn't
+                    the newest technology that Microsoft had at the time but instead, .NET Core 3.1 was the recent one.
+                    So, obviously, I went with .NET Core 3.1, mainly because I like exploring new stuff and because my
+                    teacher challenged me to do it
+                </p>
+                <p className="project-details__desc-para">
+                    The project has user Authentication with different access levels. To implement the user
+                    authentication and authorization with different access levels, I used .NET Identity with scaffolding
+                    to achieve this. Microsoft provides really good tools like SignInManager, UserManager, RoleManager
+                    and more classes to achive this.
+                </p>
+                <p className="project-details__desc-para">
+                    With Entity framework's migrations, I was able to use seeders to define default different access
+                    levels that would allow some users like admins, and managers to perform management actions on the
+                    products
+                </p>
+                <p className="project-details__desc-para">
+                    I also used a lot of javascript code to perform AJAX calls on the datatables that were accessible to
+                    managers and higher, for a more smooth experience
+                </p>
+                <h3 className="project-details__content-title">Features</h3>
+                <p className="project-details__desc-para">• User can login and register</p>
+                <p className="project-details__desc-para">• User can add products in their shopping carts to</p>
+                <p className="project-details__desc-para">
+                    • Admins and Managers management dashboard where they can perform crud actions on the products in
+                    the database
+                </p>
+                <p className="project-details__desc-para">
+                    • Admins have a dashboard area where they can asign different roles to users
+                </p>
+                <h3 className="project-details__content-title">Challenges</h3>
+                <p className="project-details__desc-para">
+                    • During my classes, we learned .NET Framework but not .NET Core. This was very challenging for me
+                    because this was one of the first time I had to learn the hard way.
+                </p>
+                <p className="project-details__desc-para">
+                    • I had to read a lot of documentation to understand how the new .NET Core framework worked. This is
+                    because I wasn't used to reading documentation
+                </p>
+
+                <h3 className="project-details__content-title">What I liked</h3>
+                <p className="project-details__desc-para">
+                    • Using .NET Core introduced me to Razer webpages which is a really nice way to integrate HTML and
+                    C# code
+                </p>
+                <p className="project-details__desc-para">
+                    • This project allowed me to expand my database design skills. (relationsips, indexes, primary keys,
+                    etc...)
+                </p>
+                <p className="project-details__desc-para">
+                    • This project gave me an idea of how actual companies manage their data and how the ECommerce
+                    industry works
+                </p>
             </>
         ),
-        codeLink: "",
     },
     {
         name: "apartment-manager",
@@ -41,8 +105,48 @@ export const ProjectsData: Array<DataSkeleton> = [
             "Apartment manager application with user authentication and different access level authorization through roles and claims/permissions. Users can interact with each other on the platform through a custom real-time chat app, and able to book appointments",
         technologies: ["C#", ".NET", "Javascript", "MsSql", "Bootstrap", "Visual studio", "Nuget pachage manager"],
 
-        overview: <></>,
-        codeLink: "",
+        codeLink: "https://github.com/munezaclovis/ApartmentManager",
+        overview: (
+            <>
+                <p className="project-details__desc-para">
+                    This was a school project for the last semester where each student had to build a Property rental
+                    management website. This final project was to help master the competencies required for the course.
+                    The project focused on analyzing, designing, implementing and testing an application using Visual
+                    Studio 2019, and ASP.NET Core
+                </p>
+                <h3 className="project-details__content-title">Criterias of the project</h3>
+                <ol type="a" className="project-details__desc-para">
+                    <li>
+                        <span>Website must have a product Owner/Administrator</span>
+                        <ol type="i">
+                            <li>Create/Update/Delete/Search/List any property manager account</li>
+                            <li>Update/Delete/Search/List any potential tenant account</li>
+                            <li>Full control of the Web Site</li>
+                        </ol>
+                    </li>
+                    <li>
+                        <span>Product Manager</span>
+                        <ol type="i">
+                            <li>Perform CRUD operations related to buildings</li>
+                            <li>Perform CRUD operations related to apartments</li>
+                            <li>Keep track of apartments status</li>
+                            <li>Schedule potential tenants appointments</li>
+                            <li>Respond to potential tenants messages</li>
+                            <li>Report any events to the property owner when necessary</li>
+                        </ol>
+                    </li>
+                    <li>
+                        <span>Pontential Tenants</span>
+                        <ol type="i">
+                            <li>Create an on-line account through the Website</li>
+                            <li>View any apartment suitable for their needs</li>
+                            <li>Make an appointment with the property manager</li>
+                            <li>Send necessary messages to the property manager</li>
+                        </ol>
+                    </li>
+                </ol>
+            </>
+        ),
     },
     {
         name: "raymonds",
@@ -64,7 +168,7 @@ export const ExperienceData: Array<DataSkeleton> = [
         position: `Full stack web developer - (January, 2022 - Present)`,
         image: MercuriLogo,
         summary:
-            "Develop visually appealing front end website architecture, including translating designer mock-ups andwireframes into front-end code with Angular and React, connect the front-end to RESTful services andAPIs developed in PHP and NodeJS with NestJS, and develop functional databases with MongoDB tosupport the back end",
+            "Develop visually appealing front end website architecture, including translating designer mock-ups and wireframes into front-end code with Angular and React, connect the front-end to RESTful services and APIs developed in PHP and NodeJS with NestJS, and develop functional databases with MongoDB to support the back end",
         liveLink: "https://mercuri.gg",
         technologies: [
             "NodeJS",
@@ -114,10 +218,10 @@ export const ExperienceData: Array<DataSkeleton> = [
     {
         name: "planetrate",
         title: "PlanetRate",
-        position: `Full stack web developer Intern- (April, 2021 - June, 2021)`,
+        position: `Full stack web developer Intern - (April, 2021 - June, 2021)`,
         image: PlanetRateLogo,
         summary:
-            "Develop and manage well-functioning PHP powered website which used CakePHP as the framework,while using Bootstrap and vanilla JavaScript to support and enhance user experience.",
+            "Develop and manage well-functioning PHP powered website which used CakePHP as the framework, while using Bootstrap and vanilla JavaScript to support and enhance user experience.",
         technologies: [
             "PHP",
             "CakePHP",
